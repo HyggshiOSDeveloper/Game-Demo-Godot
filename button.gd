0b49fc6c2f666e88f007fb100415cd268fe43767
Hyggshi OS <tfhy5321@gmail.com>
2026-07-21 12:57:52 +0700

    Add files via upload

--- button.gd
+++ button.gd
@@ -3,3 +3,8 @@ extends Node
 func _on_pressed() -> void:
 	var sniper = get_node("/root/Control/Node2D/Sniper")
 	sniper.visible = !sniper.visible
+
+	var panel = get_node("/root/Control/Panel")
+	var panel2 = get_node("/root/Control/Panel2")
+	panel.visible = !panel.visible
+	panel2.visible = !panel2.visible
